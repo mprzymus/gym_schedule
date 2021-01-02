653fedc58fefb52474938425b527457b51c93d61
mprzymus <przymus@gmail.com>
2021-01-02 19:19:26 +0100

    login added

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from . import calendar_view
+from . import calendar_view, views
 
 urlpatterns = [
     path('<int:year>/<int:month>/calendar', calendar_view.CalendarView.as_view(), name='calendar'),
+    path('login', views.user_login, name='login'),
+    path('login_check', views.login_progress, name='login_check'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,28 @@
-from django.shortcuts import render
+import datetime
 
-# Create your views here.
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+
+def __get_current_month():
+    now = datetime.datetime.now()
+    month = now.month - 1
+    year = now.year
+    return year, month
+
+
+def login_progress(request):
+    username = request.POST['login']
+    password = request.POST['pass']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        year, month = __get_current_month()
+        return redirect('/%d/%d/calendar' % (year, month))
+    else:
+        return HttpResponse('nie ma')
+
+
+def user_login(request):
+    return render(request, 'app/login.html')
