a48701056f3feb9838c73abe8c165e511c81a5ce
mprzymus <przymus@gmail.com>
2021-01-02 16:54:49 +0100

    View tests added

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class CalendarTest(TestCase):
+    client = Client()
+
+    def test_january_date(self):
+        response = self.client.get('/2020/00/calendar')
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(1, response.context['nextMonth'])
+        self.assertEqual(2020, response.context['nextYear'])
+        self.assertEqual(11, response.context['previousMonth'])
+        self.assertEqual(2019, response.context['previousYear'])
+
+    def test_december_date(self):
+        response = self.client.get('/2020/11/calendar')
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(0, response.context['nextMonth'])
+        self.assertEqual(2021, response.context['nextYear'])
+        self.assertEqual(10, response.context['previousMonth'])
+        self.assertEqual(2020, response.context['previousYear'])
