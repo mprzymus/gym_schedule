77f3770396f14b42861d370f701e245cb1ec7fdc
mprzymus <przymus@gmail.com>
2021-01-04 15:28:12 +0100

    User registration added

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('<int:year>/<int:month>/calendar', calendar_view.CalendarView.as_view(), name='calendar'),
     path('login', views.user_login, name='login'),
     path('login_check', views.login_progress, name='login_check'),
+    path('register', views.register, name='register'),
+    path('register_process', views.register_process, name='register_process'),
+    path('index', views.user_login, name='index'),
+    path('', views.user_login, name='default'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
@@ -26,3 +27,24 @@ def login_progress(request):
 
 def user_login(request):
     return render(request, 'app/login.html')
+
+
+def register(request):
+    return render(request, 'app/registration.html')
+
+
+def register_process(request):
+    username = request.POST['login']
+    password = request.POST['pass']
+    name = request.POST['name']
+    lastname = request.POST['lastname']
+    mail = request.POST['mail']
+    user = User.objects.get(username=username)
+    if user is None:
+        user = User.objects.create_user(username, mail, password)
+        user.first_name = name
+        user.last_name = lastname
+        user.save()
+        return redirect("/")
+    else:
+        return register(request)
