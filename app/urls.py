21c68f3de4aeca79b440754fc52bd1c8921c18c8
mprzymus <przymus@gmail.com>
2021-01-07 09:36:39 +0100

    Added required_login decorator to calendar view

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from . import calendar_view, views
 
 urlpatterns = [
-    path('<int:year>/<int:month>/calendar', calendar_view.CalendarView.as_view(), name='calendar'),
+    path('<int:year>/<int:month>/calendar', login_required(calendar_view.CalendarView.as_view()), name='calendar'),
     path('login', views.user_login, name='login'),
     path('login_check', views.login_progress, name='login_check'),
     path('register', views.register, name='register'),
