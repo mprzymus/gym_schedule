a164c852f0b90a16b949c94929f9cf510325384d
mprzymus <przymus@gmail.com>
2021-01-09 12:38:11 +0100

    model for exercises created

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.models import Exercise, ExerciseUsage
+
+admin.site.register(Exercise)
+admin.site.register(ExerciseUsage)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+
+
+class ExerciseUsage(models.Model):
+    exercise_id = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    weight = models.IntegerField()
+    repetitions = models.IntegerField()
+
+    class Meta:
+        unique_together = ["user_id", "date", "exercise_id"]
