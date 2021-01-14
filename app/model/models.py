f0536192508ed9e1b488f9238c29815d07df6cce
mprzymus <przymus@gmail.com>
2021-01-14 21:33:37 +0100

    added unique constraint

--- app/model/models.py
+++ app/model/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Exercise(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     description = models.CharField(max_length=1000)
 
 
