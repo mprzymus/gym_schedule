7de8a6528082eca8d155c029f6cd70d66a443b3b
mprzymus <przymus@gmail.com>
2021-01-21 22:56:53 +0100

    Period copying backend

--- app/service/day_copy_service.py
+++ app/service/day_copy_service.py
@@ -1,11 +1,77 @@
+from datetime import date
 from typing import List
+
+import datetime as dt
 
 from app.model.models import ExerciseUsage
 from app.service.exercise_usage_service import find_exercises_for_day, add_usage
 
 
 def copy_day(date_from, date_to, user):
-    ExerciseUsage.objects.filter(user_id=user, date=date_to).delete()
+    clear_day(date_to, user)
     exercises: List[ExerciseUsage] = find_exercises_for_day(date_from, user)
     for exercise in exercises:
-        add_usage(date_to, exercise.exercise_id.name, exercise.repetitions, exercise.sets, exercise.weight, exercise.user_id)
+        add_usage(date_to, exercise.exercise_id.name, exercise.repetitions, exercise.sets, exercise.weight,
+                  exercise.user_id)
+
+
+def clear_day(date_to, user):
+    ExerciseUsage.objects.filter(user_id=user, date=date_to).delete()
+
+
+def generate_dates_list(start, end):
+    date_range = end - start
+    dates = []
+    for days in range(date_range.days + 1):
+        dates.append(start + dt.timedelta(days))
+    return dates
+
+
+class PeriodCopier:
+    min_repetitions: int
+    max_repetitions: int
+    repetitions_change = 0
+    weight_up: float
+    periods_to_change = 1
+
+    def __init__(self, pattern_starts: date, pattern_length: int, copy_start: date, copy_end: date, user):
+        self.pattern_starts = pattern_starts
+        self.pattern_length = pattern_length
+        self.copy_start = copy_start
+        self.copy_end = copy_end
+        self.user = user
+
+    def generate_future_dates_list(self):
+        return generate_dates_list(self.copy_start, self.copy_end)
+
+    def generate_pattern_dates_list(self):
+        last_pattern_day = self.pattern_starts + dt.timedelta(days=self.pattern_length - 1)
+        return generate_dates_list(self.pattern_starts, last_pattern_day)
+
+    def fill_days(self):
+        i = 0
+        dates = self.generate_future_dates_list()
+        pattern_dates = self.generate_pattern_dates_list()
+        for day in dates:
+            clear_day(day, self.user)
+            source_date = pattern_dates[i % self.pattern_length]
+            current_repetition_change = int(i / self.periods_to_change) * self.repetitions_change
+            self.copy_day(day, source_date, current_repetition_change)
+            i = i + 1
+
+    def copy_day(self, target_date, source_date, repetitions_to_add):
+        exercises: List[ExerciseUsage] = find_exercises_for_day(source_date, self.user)
+        for exercise in exercises:
+            exercise = self.add_step_to_exercise(exercise, repetitions_to_add)
+            add_usage(target_date, exercise.exercise_id.name, exercise.repetitions, exercise.sets, exercise.weight,
+                      exercise.user_id)
+
+    def add_step_to_exercise(self, exercise: ExerciseUsage, repetitions_to_add) -> ExerciseUsage:
+        exercise.repetitions = exercise.repetitions + repetitions_to_add
+        while exercise.repetitions > self.max_repetitions:
+            self.change_parameters(exercise)
+        return exercise
+
+    def change_parameters(self, exercise: ExerciseUsage):
+        exercise.weight = exercise.weight + self.weight_up
+        exercise.repetitions = exercise.repetitions - self.max_repetitions + self.min_repetitions - 1
--- app/tests_day_copy_service.py
+++ app/tests_day_copy_service.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 
 from app.model.models import Exercise, ExerciseUsage
-from app.service.day_copy_service import copy_day
+from app.service.day_copy_service import copy_day, PeriodCopier
 from app.service.exercise_usage_service import find_exercises_for_day
 
 
@@ -25,7 +25,6 @@ class DayCopyServiceTest(TestCase):
         usage.save()
 
     def test_copy(self):
-
         copy_day(self.date, self.date_future, self.user)
         new_exercises = find_exercises_for_day(self.date_future, self.user)
 
@@ -57,3 +56,79 @@ class DayCopyServiceTest(TestCase):
         copy_day(self.date, self.date_future, self.user)
 
         self.assertEqual(1, len(find_exercises_for_day(self.date_future, user2)))
+
+
+class PeriodCopierTest(TestCase):
+    expected_size = 14
+
+    def setUp(self) -> None:
+        self.user = User.objects.create_user('testUser', 'test@test.com', 'testpass')
+        start_date = datetime(year=2020, month=6, day=1)
+        length = 7
+        self.last_pattern_day = datetime(year=2020, month=6, day=length)
+        self.copy_to = datetime(year=2020, month=6, day=8)
+        self.copy_until = datetime(year=2020, month=6, day=21)
+        self.copier = PeriodCopier(start_date, length, self.copy_to, self.copy_until, self.user)
+        self.copier.max_repetitions = 12
+        self.copier.min_repetitions = 8
+        self.copier.weight_up = 2.5
+
+    def test_period_generate(self):
+        period = self.copier.generate_future_dates_list()
+
+        self.assertEqual(self.expected_size, len(period))
+
+        first = period[0]
+        last = period[self.expected_size - 1]
+
+        self.assertEqual(self.copy_to, first)
+        self.assertEqual(self.copy_until, last)
+
+    def test_pattern_dates_generate(self):
+        dates = self.copier.generate_pattern_dates_list()
+
+        first = dates[0]
+        last = dates[self.copier.pattern_length - 1]
+
+        first_should_be = self.copier.pattern_starts
+        last_should_be = self.last_pattern_day
+
+        self.assertEqual(self.copier.pattern_length, len(dates))
+        self.assertEqual(first_should_be, first)
+        self.assertEqual(last_should_be, last)
+
+    def test_exercise_add(self):
+        repetitions = self.copier.max_repetitions - 1
+        weight = 10
+        exercise = ExerciseUsage()
+        exercise.repetitions = repetitions
+        exercise.weight = weight
+
+        result = self.copier.add_step_to_exercise(exercise, 1)
+
+        self.assertEqual(repetitions + 1, result.repetitions)
+        self.assertEqual(weight, result.weight)
+
+    def test_exercise_add_one_weight_up(self):
+        repetitions = self.copier.max_repetitions
+        weight = 10
+        exercise = ExerciseUsage()
+        exercise.repetitions = repetitions
+        exercise.weight = weight
+
+        result = self.copier.add_step_to_exercise(exercise, 1)
+
+        self.assertEqual(self.copier.min_repetitions, result.repetitions)
+        self.assertEqual(weight + self.copier.weight_up, result.weight)
+
+    def test_exercise_add_many_weight_up(self):
+        repetitions = self.copier.max_repetitions
+        weight = 10
+        exercise = ExerciseUsage()
+        exercise.repetitions = repetitions
+        exercise.weight = weight
+
+        result = self.copier.add_step_to_exercise(exercise, 9)
+
+        self.assertEqual(11, result.repetitions)
+        self.assertEqual(weight + 2 * self.copier.weight_up, result.weight)
