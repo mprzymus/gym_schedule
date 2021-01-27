c3109daf803270e5c65a006e7c13d8991d722883
mprzymus <przymus@gmail.com>
2021-01-27 04:03:19 +0100

    day copy

--- app/service/coach_service.py
+++ app/service/coach_service.py
@@ -2,5 +2,4 @@ from app.const import COACH_GROUP
 
 
 def is_coach(user):
-    print(user.groups.filter(name=COACH_GROUP))
     return COACH_GROUP in map(lambda group: group.name, user.groups.filter(name=COACH_GROUP))
--- app/views/day_copy.py
+++ app/views/day_copy.py
@@ -1,17 +1,21 @@
 from datetime import datetime
 
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from app.service.day_copy_service import copy_day
 from app.const import POST_DATE_FORMAT
+from app.views.user_access_service import can_user_perform
 
 
 @login_required
 def copy_day_view(request, **kwargs):
+    user = User.objects.filter(id=kwargs['usr_id']).get()
     date = request.POST['date']
-    date = datetime.strptime(date, POST_DATE_FORMAT)
     source_date = datetime.strptime(request.POST['source_date'], "%d.%m.%Y")
-    copy_day(source_date, date, request.user)
-    return HttpResponseRedirect(reverse('details', args=(source_date.year, source_date.month, source_date.day)))
+    if can_user_perform(request.user, user.id):
+        date = datetime.strptime(date, POST_DATE_FORMAT)
+        copy_day(source_date, date, user)
+    return HttpResponseRedirect(reverse('details', args=(source_date.year, source_date.month, source_date.day, user.id)))
